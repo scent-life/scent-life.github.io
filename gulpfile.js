b10eb7e1351e11b2edfedb652c5581bbcd07c301
赫门 <yuchiang.lyc@alibaba-inc.com>
2014-11-25 03:51:47 +0800

    add gulp / less / livereload

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    livereload = require('gulp-livereload');
+
+var homePath  = './',
+    stylePath = homePath + '/css';
+
+gulp.task('less', function() {
+    return gulp.src(stylePath + '/*.less')
+        .pipe(less())
+        .pipe(gulp.dest(stylePath));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(stylePath + '/*.less', ['less']);
+    gulp.watch(stylePath + '/*.css').on('change', livereload.changed);
+    gulp.watch(homePath  + '/*.html').on('change', livereload.changed);
+});
+
+gulp.task('default', [
+    'less',
+    'watch'
+]);
